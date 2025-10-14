ac2af35bac95c50940e6479e7d226ee2d356efd0
20220153 <kim0822@kumoh.ac.kr>
2025-10-14 11:47:02 +0900

    🌐한국어

---
+++ src/api/pokemonApi.js
@@ -0,0 +1,24 @@
+import { useQuery } from '@tanstack/react-query'
+import axios from 'axios'
+
+const api = axios.create({
+  baseURL: 'https://pokeapi.co/api/v2',
+})
+
+export const getPokemonList = async (limit = 30) => {
+  const res = await api.get(`/pokemon?limit=${limit}`)
+  return res.data.results
+}
+
+export const getPokemonDetail = async (name) => {
+  const res = await api.get(`/pokemon/${name}`)
+  return res.data
+}
+
+export const getPokemonSpecies = async (id) => {
+  const res = await api.get(`/pokemon-species/${id}`)
+  return res.data
+}
+// useState
+// useEffect
+// useQuery->어딘가 접속을 할 때 유용
